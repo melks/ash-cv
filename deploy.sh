63807162ee2ee0bd0de9a3d27a24fd60e85666b5
Ashley Hindmarsh <ash+github@best-scarper.co.uk>
2015-09-10 22:11:51 +0100

    Copied deploy.sh from steveklabnik

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd out
+
+git init
+git config user.name "Ashley Hindmarsh"
+git config user.email "ash+github@best-scarper.co.uk"
+
+git remote add upstream "https://$GH_TOKEN@github.com/bestscarper/ash-cv.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
